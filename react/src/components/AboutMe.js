74b3384069e8c660195c59a533cb421a6950daaa
ryhornalehach <ryhornalehach@icloud.com>
2017-12-27 11:24:37 -0500

    worked on styling

--- react/src/components/AboutMe.js
+++ react/src/components/AboutMe.js
@@ -4,7 +4,7 @@ const AboutMe = props => {
 
   return (
     <div>
-      Hello dear guests, welcome to my personal page. My name is Greg and I am web-developer. I build custom web-applications and in my work I use Ruby on Rails and React.js.
+      Hello dear guests, welcome to my personal page. My name is Greg and I am web-developer. I build custom web-applications and here you can find out more about my projects.
     </div>
   )
 }
