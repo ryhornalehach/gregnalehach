90a826e7871fe9bde541d6c57ab8933188a1a15b
ryhornalehach <ryhornalehach@icloud.com>
2018-01-15 14:20:39 -0500

    project's video added

--- react/src/components/ProjectTile.js
+++ react/src/components/ProjectTile.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const ProjectTile = props => {
-  let photo;
+  let photo, video;
   if (props.project.photo) {
-    photo = <img src={props.project.photo} width="100%"></img>
+    photo = <img src={props.project.photo} width="100%"></img>;
+  }
+  if (props.project.video) {
+    photo = null;
+    video = <iframe src={props.project.video} width="640" height="360" className="centered"></iframe>;
   }
 
   return (
@@ -12,9 +16,16 @@ const ProjectTile = props => {
         <i className="fa fa-cog fa-lg fa-spin" aria-hidden="true"></i> {props.project.name}
       </div>
       <div className='large-120'>
-        <p><a href={props.project.url}>{props.project.url}</a></p>
-        {photo}
-        {props.project.description}
+        <div className='row'>
+          <p><a href={props.project.url}>{props.project.url}</a></p>
+        </div>
+        <div className='row'>
+          {photo}
+          {video}
+        </div>
+        <div className='row'>
+          {props.project.description}
+        </div>
       </div>
       <hr/>
     </div>
